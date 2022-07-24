30b5fc28c3afc126f5a680226e394a0bc9630643
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-07-23 21:51:55 -0300

    starting implementation of heap

---
+++ src/heap/BinaryHeap.ts
@@ -0,0 +1,20 @@
+class BinaryHeap<T>
+{
+    private items: Array<T>;
+    private size: number;
+
+    constructor(capacity: number) {
+        this.size = 0;
+        this.items = new Array(capacity);
+    }
+
+    public insert = () => {
+
+    }
+
+    public remove = () => {
+
+    }
+
+    public 
+}
